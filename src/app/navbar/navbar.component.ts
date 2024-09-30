541d218eac53000731c217bc8d7ef1207c4b1a60
Jonathan ASSAMAGAN <80401029+kuro-jojo@users.noreply.github.com>
2024-09-30 13:51:16 +0200

    Add internationilization

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [NgClass],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+    currentLang: string = window.location.pathname.split('/')[1];
 
+    switchLanguage(language: string, event: Event) {
+        event.preventDefault();
+        this.currentLang = language;
+        const newUrl = `/${language}`;
+        window.location.href = newUrl;
+    }
 }
