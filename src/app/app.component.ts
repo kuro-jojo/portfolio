888fb784f62d74740473c5f0ddb18fed24ddc928
Jonathan ASSAMAGAN <80401029+kuro-jojo@users.noreply.github.com>
2025-02-15 21:27:27 +0100

    Change portfolio template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from "./contact/contact.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AboutMeComponent, ProjectsComponent, ContactComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, NavbarComponent, AboutMeComponent, ProjectsComponent, ContactComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+    title = 'portfolio';
+    showArrow = false;
+
+    @HostListener('window:wheel', [])
+    onWindowScroll() {
+        this.showArrow = window.scrollY > 20;
+    }
 }
