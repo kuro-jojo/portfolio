cf712ccafcb8267e8bdee2c62687c423cc5b878c
Jonathan Assamagan <kurojojo@gmail.com>
2026-02-02 14:11:48 +0100

    upgrade angular to v21 to fix security vulnerabilities

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -4,7 +4,6 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-about-me',
-    standalone: true,
     imports: [SafePipe, MatIconModule],
     templateUrl: './about-me.component.html',
     styleUrl: './about-me.component.css'
